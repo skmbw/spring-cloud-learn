c96c5bd7d729d5f55e5b95549b39eacdbe81b10d
skmbw <tongku2008@126.com>
2017-12-12 16:03:38 +0800

    为 service-feign，增加断路器监控

--- service-feign/src/main/java/com/fuuzii/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/fuuzii/ServiceFeignApplication.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrixDashboard // 开启断路器监控
+@EnableHystrix // feign 开启断路器不需要这个注解，但是断路器监控需要，所以加上
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
